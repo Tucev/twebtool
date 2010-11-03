3dad5446e271de12f22bd23285afd5be01a46cf3
eloygbm <eloygbm@10ae5230-7d50-de86-7229-d430a4c520b7>
2010-11-03 18:41:21 +0000

    Issue #7 Import Masivo

--- grails-app/services/twebtool/ClaveService.groovy
+++ grails-app/services/twebtool/ClaveService.groovy
@@ -22,4 +22,38 @@ class ClaveService {
 
         return claveInstance
     }
+
+
+    def initialImport(Properties props, Proyecto proyecto){
+        
+        def idiomaDefault = initializeData(proyecto)
+        props.propertyNames().each{
+                def claveInstance = new Clave(nombre:it,proyecto:proyecto,descripcion:'')
+                claveInstance.save()
+                claveInstance
+                        .addToTextos(new Texto(valor:props.getProperty(it),idioma:idiomaDefault))
+                        .save()
+        }
+
+    }
+
+
+    private initializeData(Proyecto proyecto){
+
+        //borramos todos los textos, claves e idiomas
+        def idiomas = Idioma.findAllByProyecto(proyecto)        
+        def claves = Clave.findAllByProyecto(proyecto)
+        idiomas.each{
+                it.delete(flush:true)
+        }
+        claves.each{
+                it.delete(flush:true)        
+        }
+
+        //creamos idioma Default
+        def i = new Idioma(nombre:"default",localeValue:"default",proyecto:proyecto)
+        i.save(flush:true)
+        return i
+    }
+
 }
